abd0e866889db53e925a77cfd456e8a22e246c51
guijunqiao <gjq3535139173@qq.com>
2025-10-07 17:52:26 +0800

    添加上响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -43,4 +43,16 @@ service.interceptors.request.use(
   }
 )
 
+/**
+ * 响应拦截器，响应数据之后，所有的then之前被调用
+ */
+service.interceptors.response.use((response)=>{
+  const {success, message, data} = response.data
+  if(success){
+    return data
+  }
+  //统一拦截错误响应
+  return Promise.reject(new Error(message))
+})
+
 export default service;
