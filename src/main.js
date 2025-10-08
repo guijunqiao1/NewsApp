875f5819f392d54c968f1da8f9473b01e21ccd92
guijunqiao <gjq3535139173@qq.com>
2025-10-08 15:15:52 +0800

    动态指定rem基准值

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router";//自动索引到其中的index.js
+import { useREM } from './utils/flexible'
+
 
 createApp(App).use(router).mount('#app');
+
+//设置根元素的字体大小便于使用rem单位
+useREM()
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -13,3 +13,22 @@ const { width, height } = useWindowSize();//获取当前实际设备屏幕的宽
 export const isMobile = computed(() => {//比较后计算当前实际宽高和界定值的大小关系而后返回计算属性值
   return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ * rem解释：
+   1、rem 代表 "Root EM"，即“根元素的相对单位”。
+   2、它是 相对单位，与 em 类似，但 rem 始终是基于文档根元素 (<html>) 的字体大小，而 em 则是相对于它的父元素的字体大小。
+ * 动态指定 rem 基准值，最大40px
+ * 根据屏幕宽度计算，赋值给html：fontsize
+ */
+export const useREM = ()=>{
+  // 定义最大的fontsize
+  const MAX_FONT_SIZE = 40  
+  // 监听 html 文档被解析完成的事件
+  document.addEventListener('DOMContentLoaded',()=>{
+    const html = document.querySelector('html')
+    // 计算 fontSize, 根据屏幕宽度 / 10  解释：window.innerWidth是窗口内容区域的宽度
+    let fontSize = Math.min(MAX_FONT_SIZE, window.innerWidth / 10)
+    html.style.fontSize = fontSize + 'px'
+  })
+}
