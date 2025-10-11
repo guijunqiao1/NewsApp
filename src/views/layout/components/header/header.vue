30a5ea947d1bc626260d63a54bd0ee4e5e844450
桂军桥 <gjq3535139173@qq.com>
2025-10-11 19:33:49 +0800

    header首部处理完毕--即完成home图标以及header的挂载

--- src/views/layout/components/header/header.vue
+++ src/views/layout/components/header/header.vue
@@ -1,6 +1,20 @@
 <template>
-
-
+  <!-- 头部框架 -->
+  <div
+    class="w-full bg-white dark:bg-zinc-800 border-b border-b-zinc-200 dark:border-b-zinc-700 px-2 py-1 duration-500"
+  >
+    <div class="flex items-center">
+      <img
+        v-lazy
+        src="@/assets/icons/news.svg"
+        class="h-4 cursor-pointer mr-2 guide-home"
+        @click="toHome"
+      />
+      <header-search-vue></header-search-vue>
+      <header-theme-vue></header-theme-vue>
+      <header-my-vue></header-my-vue>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,8 +24,19 @@
   import { useRouter } from 'vue-router'
 
 
+  const router = useRouter();//首部路由切换图标方法存放对象的获取
+
+  const to_Home = ()=>{
+    console.log("go home");
+    router.push('/');
+    // '@/views/layout/index.vue'--与home组建对应
+  }
+
+
 </script>
 
 <style lang="scss">
+
+
 
 </style>
