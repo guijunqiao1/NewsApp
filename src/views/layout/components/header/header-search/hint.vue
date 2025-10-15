35faeb861bf87494a0971d268f2bf608a5549837
桂军桥 <gjq3535139173@qq.com>
2025-10-15 16:53:11 +0800

    正则匹配搜索关键字，高亮处理

--- src/views/layout/components/header/header-search/hint.vue
+++ src/views/layout/components/header/header-search/hint.vue
@@ -4,9 +4,9 @@
       v-for="(item, index) in hintData"
       :key="index"
       class="py-1 pl-1 text-base font-bold text-zinc-500 rounded cursor-pointer duration-300 hover:bg-zinc-200 dark:hover:bg-zinc-900"
-      @click="onItemClick(item)"
+      @click="onItemClick(item)"     
+      v-html="highlightText(item)"
     >
-      {{ item }}
     </div>
   </div>
 </template>
@@ -63,4 +63,21 @@
     const onItemClick = (item) => {
         emits('itemClick', item)
     }
+
+
+    /**
+     * 处理关键字高亮
+     */
+    const highlightText = (text) => {
+      // 生成高亮标签
+      const highlightStr = `<span class="text-zinc-900 dark:text-zinc-200">${props.searchText}</span>`
+      // 构建正则表达式，从《显示文本中》找出与《用户输入文本相同的内容》，使用《高亮标签》进行替换
+      const reg = new RegExp(props.searchText, 'gi')
+      // 替换
+      return text.replace(reg, highlightStr)
+    }
+
+
+
+
 </script>
