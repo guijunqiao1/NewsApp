bff81c39344dd8768677d00999de53f73c4353f2
桂军桥 <gjq3535139173@qq.com>
2025-10-16 17:23:59 +0800

    初步构建message

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,15 @@ module.exports = {//没有设置type:module故使用commonjs正常--没有设置
       colors:{
         main: '#f44c58',
         'hover-main': '#f32836',
+        'success-100': '#F2F9EC',
+        'success-200': '#E4F2DB',
+        'success-300': '#7EC050',
+        'warn-100': '#FCF6ED',
+        'warn-200': '#F8ECDA',
+        'warn-300': '#DCA550',
+        'error-100': '#ED7456',
+        'error-200': '#f3471c',
+        'error-300': '#ffffff'
       }
     }
   },
