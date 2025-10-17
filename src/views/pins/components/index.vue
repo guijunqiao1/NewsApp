c473bd2b6143ad6dee436510c5025d626391b74b
桂军桥 <gjq3535139173@qq.com>
2025-10-17 17:09:11 +0800

    构建navbar(mobile使用),应用于pins的mobile端

--- src/views/pins/components/index.vue
+++ src/views/pins/components/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="fixed left-0 top-0 w-screen h-screen text-xl bg-slate-500 z-20">
+    <!-- mobile的pins顶部 -->
+    <m-navbar></m-navbar>
+
+    <!-- pc的pins顶部 -->
+
+    <!-- 公共内容区 -->
     {{ id }}
   </div>
 </template>
@@ -13,5 +19,4 @@
     }
     })
 
-    console.log("props:",props.id);
 </script>
