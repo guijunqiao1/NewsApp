0528024348886621a3e5889c6cf59e8ccc474f8f
桂军桥 <gjq3535139173@qq.com>
2025-10-20 19:24:14 +0800

    构建通用组件 trigger-menu和 trigger-menu-item--触发式菜单组件

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -5,11 +5,40 @@
     <div class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500">
       <list-vue></list-vue>
     </div>
+    <m-trigger-menu
+      v-if="isMobile"
+      class="fixed bottom-6 m-auto left-0 right-0 w-[220px]"
+    >
+      <m-trigger-menu-item
+        icon="home"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+      >
+        首页
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        v-if="$store.getters.token"
+        icon="vip"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onVipClick"
+      >
+        VIP
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        icon="profile"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onMyClick"
+      >
+        {{ $store.getters.token ? '我的' : '去登录' }}
+      </m-trigger-menu-item>
+    </m-trigger-menu>
   </div>
 </template>
 
 <script setup>
   import navigation from "./components/navigation/index.vue";
+  import { isMobile } from '@/utils/flexible'
   //应用瀑布流架构
   import listVue from './components/list/index.vue'
 
