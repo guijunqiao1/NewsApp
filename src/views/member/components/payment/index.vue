878fc089b8bf290c0f4e589f7c082e3679e7fedb
桂军桥 <gjq3535139173@qq.com>
2025-10-21 16:21:24 +0800

    完成移动端支付样式

--- src/views/member/components/payment/index.vue
+++ src/views/member/components/payment/index.vue
@@ -2,10 +2,14 @@
   <div>
     <!-- PC 端支付 -->
     <pc-payment-vue v-if="!isMobile"></pc-payment-vue>
+    <!-- 移动端支付 -->
+    <mobile-payment-vue v-else></mobile-payment-vue>
+
   </div>
 </template>
 
 <script setup>
     import { isMobile } from '@/utils/flexible'
     import pcPaymentVue from './pc-payment/index.vue'
+    import mobilePaymentVue from './mobile-payment/index.vue'
 </script>
