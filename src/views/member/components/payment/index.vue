8483be21157493725cf30f9b451f01c0315c9910
桂军桥 <gjq3535139173@qq.com>
2025-10-21 16:04:21 +0800

    构建pc端支付样式

---
+++ src/views/member/components/payment/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- PC 端支付 -->
+    <pc-payment-vue v-if="!isMobile"></pc-payment-vue>
+  </div>
+</template>
+
+<script setup>
+    import { isMobile } from '@/utils/flexible'
+    import pcPaymentVue from './pc-payment/index.vue'
+</script>
