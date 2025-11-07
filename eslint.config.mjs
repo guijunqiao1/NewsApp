91d8c794d3018aa8e11f79afa348ee8a6e6f49d8
桂军桥 <gjq3535139173@qq.com>
2025-11-07 19:29:47 +0800

    成功配置上eslint基本框架

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } 
+  },
+  {
+		rules: {
+			"no-console": "warn",
+		},
+	},
+  pluginVue.configs["flat/essential"],
+]);
--- src/views/login-register/login/qq-login.vue
+++ src/views/login-register/login/qq-login.vue
@@ -30,7 +30,7 @@ onMounted(() => {
       btnId: 'qqLoginBtn' //插入按钮的节点id
     },
     // 登录成功之后的回调，但是需要注意，这个回调只会在《登录回调页面中被执行》
-    // 登录存在缓存，登录成功一次之后，下次进入会自动重新登录（即：触发该方法，所以我们应该在离开登录页面时，注销登录）
+    // 登录存在缓存，登录成功一次之后，下次进入会自动重新登录（即：触发该方法，我们应该在离开登录页面时，注销登录）
     (data, opts) => {
       console.log('QQ登录成功')
       // 1. 注销登录，否则在后续登录中会直接触发该回调
@@ -38,7 +38,17 @@ onMounted(() => {
       // 2. 获取当前用户唯一标识，作为判断用户是否已注册的依据
       const accessToken = /access_token=((.*))&expires_in/.exec(
         window.location.hash
+        // 补充：获取当前页面 URL 中的 哈希值（#后面的部分）。
       )[1]
+
+      // 此处进行exec方法的补充：
+      // exec() 方法是正则表达式对象的一个方法，用于执行匹配并返回匹配的结果。
+      // exec(window.location.hash) 会返回一个数组，其中：
+      // 第一个元素是匹配到的完整字符串（即 access_token=abc123&expires_in=3600）。
+      // 第二个元素是第一个捕获组的内容，也就是 access_token 后面的值（即 abc123）。
+
+      // 例如，在 URL 哈希为 #access_token=abc123&expires_in=3600 时： access_token=abc123 这个部分会被正则匹配并捕获。
+
       // 3. 拼接请求对象
       const oauthObj = {
         nickname: data.nickname,
