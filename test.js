39dcc7aa1cf5d2a358c20ed28d91826fbedb3758
OMNIFY07\omnif <gjq3535139173@qq.com>
2026-03-16 11:34:00 +0800

    test2

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 // // debounce--手写防抖:
 // // function debounce(fn,delay){
-// //   let timer = null;  
+// //   let timer = null;   
 // //   return ()=>{
 // //     if(timer){
 // //       clearTimeout(timer);
 // //     }
-// //     timer = setTimeout(()=>{
+// //     timer = setTimeout(()=>{   
 // //       fn();
 // //     },delay)
 // //   }
